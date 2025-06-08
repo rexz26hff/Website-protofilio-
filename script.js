1b46bd84e079931eaed35f7733a0361356b2b487
rexz26hff <bg185967@gmail.com>
2025-06-09 03:15:53 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function goToAnotherScreen() {
+    window.location.href = "detail.html";
+}
+function nia() {
+    window.location.href = "nia.html";
+}
+function raman() {
+    window.location.href = "raman.html";
+}
+function fataya() {
+    window.location.href = "fataya.html";
+}
+function pipin() {
+    window.location.href = "pipin.html";
+}
+function reva() {
+    window.location.href = "reva.html";
+}
